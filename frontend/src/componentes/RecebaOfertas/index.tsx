1dde602ae0ad4870cff8df639ea7f10002a3d9c0
rafaelainocencio <rafaelainocencioo@gmail.com>
2022-03-18 16:29:22 -0300

    host no RecebaOfertas

--- frontend/src/componentes/RecebaOfertas/index.tsx
+++ frontend/src/componentes/RecebaOfertas/index.tsx
@@ -19,7 +19,7 @@ export default function RecebaOfertas() {
 
 
         const config: AxiosRequestConfig = {
-            baseURL: BASE_URL,
+            baseURL: "https://recodeair.netlify.app/",
             method: 'POST',
             url: '/assinantes',
             data: {
